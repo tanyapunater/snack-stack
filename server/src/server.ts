a1c065dce52bb4617b0cf6ef2a41344932b3288f
tanya <tanya.punater@yahoo.com>
2025-04-10 20:17:48 -0400

    update server/src/server.ts again

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import path from 'node:path';
 import type { Request, Response } from 'express';
 import db from './config/connection.js'
 import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
