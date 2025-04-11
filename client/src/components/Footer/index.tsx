669b3078e2fe9b0d4846517fc973576fab85333b
tanya <tanya.punater@yahoo.com>
2025-04-10 20:44:07 -0400

    pushing changes

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -1,9 +1,4 @@
-import { useLocation, useNavigate } from "react-router-dom";
-
 const Footer: React.FC = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-
   const footerNavigation = {
     solutions: [
       { name: "Hosting", href: "#" },
@@ -94,15 +89,6 @@ const Footer: React.FC = () => {
     ],
   };
 
-  const handleGoBack = () => {
-    if (window.history.length > 1) {
-      //Check if there is a previous page in the history stack
-      navigate(-1);
-    } else {
-      navigate("/");
-    }
-  };
-
   return (
     <>
       <footer className="mx-auto min-w-6xl max-w-7xl px-6 lg:px-8">
@@ -124,28 +110,6 @@ const Footer: React.FC = () => {
           </p>
         </div>
       </footer>
-
-      {/* <footer className="w-100 mt-auto bg-secondary p-4">
-        <div className="container text-center mb-5">
-          {location.pathname !== "/" && (
-            <button className="btn btn-dark mb-3" onClick={handleGoBack}>
-              &larr; Go Back
-            </button>
-          )}
-          <h4>
-            Made with{" "}
-            <span
-              className="emoji"
-              role="img"
-              aria-label="heart"
-              aria-hidden="false"
-            >
-              ❤️
-            </span>{" "}
-            by the Tech Thoughts team.
-          </h4>
-        </div>
-      </footer> */}
     </>
   );
 };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,13 @@
-import express from 'express';
-import type { Request, Response } from 'express';
-import db from './config/connection.js'
-import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
-import { expressMiddleware } from '@apollo/server/express4';
-import { typeDefs, resolvers } from './schemas/index.js';
-import { authenticateToken } from './utils/auth.js';
+import express from "express";
+import db from "./config/connection.js";
+import { ApolloServer } from "@apollo/server"; // Note: Import from @apollo/server-express
+import { expressMiddleware } from "@apollo/server/express4";
+import { typeDefs, resolvers } from "./schemas/index.js";
+import { authenticateToken } from "./utils/auth.js";
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 const startApolloServer = async () => {
@@ -21,15 +20,16 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server as any,
-    {
-      context: authenticateToken as any
-    }
-  ));
+  app.use(
+    "/graphql",
+    expressMiddleware(server as any, {
+      context: authenticateToken as any,
+    })
+  );
 
   if (process.env.NODE_ENV === "production") {
     app.use(express.static("../client/dist"));
-  
+
     app.get("*", (_req, res) => {
       res.sendFile("../client/dist/index.html");
     });
