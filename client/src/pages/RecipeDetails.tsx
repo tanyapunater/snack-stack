910810689d3a4405476d4cfb2589da67b7f251c4
Wren West <wrenwestart@gmail.com>
2025-04-17 19:54:33 -0400

    edited RecipeDetails.tsx to include queries

--- client/src/pages/RecipeDetails.tsx
+++ client/src/pages/RecipeDetails.tsx
@@ -1,44 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getRecipeDetails } from '../components/RecipeDetails/RecipeDetails'; 
+import { useQuery } from '@apollo/client';
+import { QUERY_SINGLE_RECIPE } from '../utils/queries'; // Import the query for fetching a single recipe
 
 const RecipeDetails: React.FC = () => {
 
     const { recipeId } = useParams(); // Get the recipeId from the URL parameters
+    const { loading, data } = useQuery(QUERY_SINGLE_RECIPE, {
+        variables: { recipeId: recipeId},
+      });
+    const recipe= data?.recipe|| {};
 
-    interface Recipe {
-            id: number
-            title: string
-            description: string
-            ingredients: string[]
-            instructions: string
-            category: string
-            imageUrl: string
-          }
-
-          const [recipe, setRecipe] = useState<Recipe | null>(null);
-          const [error, setError] = useState(false);
-
-
-            const fetchRecipeDetails = async () => {
-                try {
-                    if (recipeId) {
-                        const data = await getRecipeDetails(recipeId);
-                        setRecipe({ ...data, imageUrl: data.imageUrl || '' }); // Ensure imgUrl is included
-                    } else {
-                        console.error('Recipe ID is not defined');
-                        setError(true);
-                    }
-                } catch (error) {
-                    console.error('Error fetching recipe details:', error);
-                    setError(true);
-                }
-            }
-            
-            useEffect(() => { 
-                fetchRecipeDetails();
-              }, [recipeId]);
-              
+    if (loading) {
+        return <div>Loading...</div>;
+      }
+                
+    const error = useState(false);
 
     return (
         <div className="flex flex-auto items-center justify-center flex-col w-full h-full bg-gray-100 p-4">
@@ -46,19 +23,18 @@ const RecipeDetails: React.FC = () => {
                 <div className="bg-white shadow-md rounded-lg p-6 max-w-md w-full">
                     <div className="flex flex-col items-center mb-4">
                     <h1 className="text-4xl font-extrabold mb-8 text-center mt-24 bg-gradient-to-r from-blue-500 via-sky-400 to-cyan-500 text-white py-4 rounded-xl shadow-md">
-                        {recipe.id}</h1>
-                    <h2 className="text-2xl font-bold mb-4">{recipe.title}</h2>
+                        {recipe.title}</h1>
                     <img src={recipe.imageUrl} alt={recipe.title} className="mb-4 rounded-lg" />
                     <p className="text-gray-700 mb-4">{recipe.description}</p>
                     <h3 className="text-xl font-semibold mb-2">Ingredients</h3>
                     <ul className="list-disc list-inside mb-4">
-                        {recipe.ingredients.map((ingredient, index) => (
+                        {recipe.ingredients.map((ingredient: string, index: number) => (
                             <li key={index}>{ingredient}</li>
                         ))}
                     </ul>
                     <h3 className="text-xl font-semibold mb-2">Instructions</h3>
                     <ol className="list-decimal list-inside mb-4">
-                        {recipe.instructions.split('\n').map((instruction, index) => (
+                        {recipe.instructions.split('\n').map((instruction: string, index: number) => (
                             <li key={index}>{instruction}</li>
                         ))}
                     </ol>
