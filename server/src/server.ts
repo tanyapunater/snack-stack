f81eb626f94db15f5d125607945b847e6ec49046
tanya <tanya.punater@yahoo.com>
2025-04-17 19:10:17 -0400

    server.ts changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,11 @@ import { ApolloServer } from "@apollo/server"; // Note: Import from @apollo/serv
 import { expressMiddleware } from "@apollo/server/express4";
 import { typeDefs, resolvers } from "./schemas/index.js";
 import { authenticateToken } from "./utils/auth.js";
+import { fileURLToPath } from "url";
+import path from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = new ApolloServer({
   typeDefs,
@@ -28,10 +33,12 @@ const startApolloServer = async () => {
   );
 
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../client/dist"));
+    const clientBuildPath = path.join(__dirname, "..", "..", "client", "dist");
+
+    app.use(express.static(clientBuildPath));
 
     app.get("*", (_req, res) => {
-      res.sendFile("../client/dist/index.html");
+      res.sendFile(path.join(clientBuildPath, "index.html"));
     });
   }
 
