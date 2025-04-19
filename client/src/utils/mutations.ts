9bb61d476f5d465eab07951e9a137e0d3a4ecfa1
Lakulength@1717 <girm2002@gmail.com>
2025-04-19 11:31:54 -0400

    queries and mutations are taken care of

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -1,3 +1,4 @@
+
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
@@ -52,5 +53,50 @@ export const ADD_COMMENT = gql`
         createdAt
       }
     }
+  }
+`;
+
+// Recipe mutations
+export const ADD_RECIPE = gql`
+  mutation AddRecipe($input: AddRecipeInput!) {
+    addRecipe(input: $input) {
+      _id
+      title
+      description
+      ingredients
+      instructions
+      category
+      createdAt
+      updatedAt
+      createdBy {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const UPDATE_RECIPE = gql`
+  mutation UpdateRecipe($id: ID!, $input: UpdateRecipeInput!) {
+    updateRecipe(id: $id, input: $input) {
+      _id
+      title
+      description
+      ingredients
+      instructions
+      category
+      createdAt
+      updatedAt
+      createdBy {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const DELETE_RECIPE = gql`
+  mutation DeleteRecipe($id: ID!) {
+    deleteRecipe(id: $id)
   }
 `;
