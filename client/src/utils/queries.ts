09c52b24e2978e02aca23022d3e6758eec743e27
Lakulength@1717 <girm2002@gmail.com>
2025-04-19 11:47:40 -0400

    queries and mutations added

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -1,67 +1,3 @@
-// import { gql } from '@apollo/client';
-
-// export const QUERY_USER = gql`
-//   query user($username: String!) {
-//     user(username: $username) {
-//       _id
-//       username
-//       email
-//       thoughts {
-//         _id
-//         thoughtText
-//         createdAt
-//       }
-//     }
-//   }
-// `;
-
-// export const QUERY_THOUGHTS = gql`
-//   query getThoughts {
-//     thoughts {
-//       _id
-//       thoughtText
-//       thoughtAuthor
-//       createdAt
-//     }
-//   }
-// `;
-
-// export const QUERY_SINGLE_THOUGHT = gql`
-//   query getSingleThought($thoughtId: ID!) {
-//     thought(thoughtId: $thoughtId) {
-//       _id
-//       thoughtText
-//       thoughtAuthor
-//       createdAt
-//       comments {
-//         _id
-//         commentText
-//         commentAuthor
-//         createdAt
-//       }
-//     }
-//   }
-// `;
-
-// export const QUERY_ME = gql`
-//   query me {
-//     me {
-//       _id
-//       username
-//       email
-//       thoughts {
-//         _id
-//         thoughtText
-//         thoughtAuthor
-//         createdAt
-//       }
-//     }
-//   }
-// `;
-
-
-
-
 import { gql } from '@apollo/client';
 
 export const QUERY_USER = gql`
@@ -161,3 +97,6 @@ export const QUERY_RECIPE = gql`
     }
   }
 `;
+
+// Add this line to alias QUERY_RECIPE as QUERY_SINGLE_RECIPE
+export const QUERY_SINGLE_RECIPE = QUERY_RECIPE;
